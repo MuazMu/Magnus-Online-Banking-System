a39477e74e49e951b5078f5bfde81c73f54677dc
Muaz <106611299+MuazMu@users.noreply.github.com>
2024-10-01 17:24:57 +0300

    Implemented Home button on Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,16 +1,39 @@
+'use client'
+
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
 
 const Sidebar = ({ user }: SiderbarProps ) => {
+  
+  const pathname = usePathname();
+
   return (
     <section className="sidebar">
     <nav className="flex flex-col gap-4">
-     <Link href="/" className="mb-12 cursor-pointer items-center gap-2"></Link>
+     <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
        <Image 
                   src="/icons/logo.svg"
                   width={34}
-                  height={34} alt={''}       /> 
+                  height={34} alt={'Novus Logo'}   
+                  className="size-[24px] max-xl:size-14"   />
+          <h1 className="sidebar-logo">Novus</h1>         
+      </Link>
+      {sidebarLinks.map((item)=>  
+      {
+        const isActive =
+        pathname === item.route || pathname.startsWith(`${item.route}/`)
+      return(
+        <Link href={item.route} key={item.label}
+        className={cn('sidebar-link',{'bg-bank-gradient': isActive})}
+        >
+          {item.label}
+        </Link>
+      )
+    })}
     </nav>
     </section>
  
